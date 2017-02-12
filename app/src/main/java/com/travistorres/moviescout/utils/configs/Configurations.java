befed87e822de7f5a5a6fc662f1543315f0f9286
Travis Anthony Torres <travis@travistorres.com>
2017-02-12 14:45:10 -0500

    refactor:  Configurations
    Modified Configurations so that they extend the Hashtable class.  This
    removes the need to have to override the Collections interface and return
    data from some private collection.

--- app/src/main/java/com/travistorres/moviescout/utils/configs/Configurations.java
+++ app/src/main/java/com/travistorres/moviescout/utils/configs/Configurations.java
@@ -5,31 +5,32 @@
 package com.travistorres.moviescout.utils.configs;
 
 import android.content.res.XmlResourceParser;
-import android.support.annotation.NonNull;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Hashtable;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * TODO:  document
  */
 
-class Configurations implements Map<String, String> {
-    public final static int DEFAULT_CONTENT_SIZE = 10;
+class Configurations extends Hashtable<String, String> {
+    public static  final int DEFAULT_CONTENT_SIZE = 10;
 
-    private final static String RESOURCE_DELIMITER  = ".";
-    private final static String INVALID_PARSER_SUPPLIED_MESSAGE = "XmlResourceParser provided was not set to the start of the document";
-
-    private Hashtable<String, String> contents;
+    private static final String RESOURCE_DELIMITER  = ".";
+    private static final String INVALID_PARSER_SUPPLIED_MESSAGE = "XmlResourceParser provided was not set to the start of the document";
+    private static final int RESOURCE_NAME_OFFSET_POSITION = 1;
+    private static final int RESOURCE_NAME_START_POSITION = 0;
 
     public Configurations(XmlResourceParser parser)
             throws XmlPullParserException, IllegalArgumentException, IOException {
-        contents = new Hashtable<String, String>(DEFAULT_CONTENT_SIZE);
+        this(parser, DEFAULT_CONTENT_SIZE);
+    }
+
+    public Configurations(XmlResourceParser parser, int contentSize)
+            throws XmlPullParserException, IllegalArgumentException, IOException {
+        super(contentSize);
 
         buildConfigurations(parser);
     }
@@ -42,7 +43,6 @@ class Configurations implements Map<String, String> {
         }
 
         String resourceName = "";
-
         while (parser.next() != XmlResourceParser.END_DOCUMENT) {
             int domEvent = parser.getEventType();
             switch (domEvent) {
@@ -51,78 +51,15 @@ class Configurations implements Map<String, String> {
                     resourceName += RESOURCE_DELIMITER + parser.getName();
                     break;
                 case XmlResourceParser.END_TAG:
-                    //  omit the previous resource from the resource name
+                    //  omit the previous resource from the resource name string
                     int separatorIndex = resourceName.lastIndexOf(RESOURCE_DELIMITER);
-                    resourceName = resourceName.substring(0, separatorIndex);
+                    resourceName = resourceName.substring(RESOURCE_NAME_START_POSITION, separatorIndex);
                     break;
                 case XmlResourceParser.TEXT:
                     //  add the configuration
-                    put(resourceName.substring(1), parser.getText());
+                    put(resourceName.substring(RESOURCE_NAME_OFFSET_POSITION), parser.getText());
                     break;
             }
         }
-    }
-
-    @Override
-    public int size() {
-        return contents.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return contents.isEmpty();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return contents.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return contents.containsValue(value);
-    }
-
-    @Override
-    public String get(Object key) {
-        return contents.get(key);
-    }
-
-    @Override
-    public String put(String key, String value) {
-        return contents.put(key, value);
-    }
-
-    @Override
-    public String remove(Object key) {
-        return contents.remove(key);
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends String> m) {
-        contents.putAll(m);
-    }
-
-    @Override
-    public void clear() {
-        contents.clear();
-    }
-
-    @NonNull
-    @Override
-    public Set<String> keySet() {
-        return contents.keySet();
-    }
-
-    @NonNull
-    @Override
-    public Collection<String> values() {
-        return contents.values();
-    }
-
-    @NonNull
-    @Override
-    public Set<Entry<String, String>> entrySet() {
-        return contents.entrySet();
     }
 }
