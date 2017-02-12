09e72799d2c617904a991d6b61690f556796e574
Travis Anthony Torres <travis@travistorres.com>
2017-02-12 15:20:10 -0500

    docs:  Configuration Documentation
    Documents the features of the Configuration Reader classes.

--- app/src/main/java/com/travistorres/moviescout/utils/configs/Configurations.java
+++ app/src/main/java/com/travistorres/moviescout/utils/configs/Configurations.java
@@ -12,22 +12,58 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 /**
- * TODO:  document
+ * Configurations
+ *
+ * Packages the contents of a given XML parser into a Hash Table for easy access to it's contents.
+ * Each configuration can be acquired by it's tag name DOM sequence.  For instance since the API V3
+ * configuration lies under the configurations > movieDbApi > apiKeyV3 tag's, it can be acquired
+ * by specifying configurations.movieDbApi.apiKeyV3 as the key.  Each config section will be
+ * delimited using the "." character.
+ *
+ * @author Travis Anthony Torres
+ * @version February 12, 2017
  */
 
 class Configurations extends Hashtable<String, String> {
-    public static  final int DEFAULT_CONTENT_SIZE = 10;
+    public static final int DEFAULT_CONTENT_SIZE = 10;
 
+    /*
+     *  Various values that are utilized for storing the contents of the configuration file into
+     *  a Collection.
+     *
+     */
     private static final String RESOURCE_DELIMITER  = ".";
     private static final String INVALID_PARSER_SUPPLIED_MESSAGE = "XmlResourceParser provided was not set to the start of the document";
     private static final int RESOURCE_NAME_OFFSET_POSITION = 1;
     private static final int RESOURCE_NAME_START_POSITION = 0;
 
+    /**
+     * Stores the contents of the configurations file into a Collection for accessing throughout
+     * the application.
+     *
+     * @param parser An XML parser which is set to read from the configuration file.
+     *
+     * @throws XmlPullParserException
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
     public Configurations(XmlResourceParser parser)
             throws XmlPullParserException, IllegalArgumentException, IOException {
         this(parser, DEFAULT_CONTENT_SIZE);
     }
 
+    /**
+     * Stores the contents of the configurations file into a Collection for accessing throughout
+     * the application.
+     *
+     * @param parser An XML parser which is set to read from the configuration file.
+     * @param contentSize The number of items that should be reserved for use within the
+     *                    Collection.
+     *
+     * @throws XmlPullParserException
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
     public Configurations(XmlResourceParser parser, int contentSize)
             throws XmlPullParserException, IllegalArgumentException, IOException {
         super(contentSize);
@@ -35,6 +71,16 @@ class Configurations extends Hashtable<String, String> {
         buildConfigurations(parser);
     }
 
+    /**
+     * Iterates through the XML document and maps each configuration to a specific key value pair
+     * in the Collection.
+     *
+     * @param parser An XML parser which is set to read from the configuration file.
+     *
+     * @throws XmlPullParserException
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
     private void buildConfigurations(XmlResourceParser parser)
             throws XmlPullParserException, IllegalArgumentException, IOException {
         //  The parser must be set to the start of the document
--- app/src/main/java/com/travistorres/moviescout/utils/configs/ConfigurationsReader.java
+++ app/src/main/java/com/travistorres/moviescout/utils/configs/ConfigurationsReader.java
@@ -14,25 +14,55 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 
 /**
- * TODO:  document
+ * ConfigurationsReader
+ *
+ * Performs static methods for accessing the contents of the Configuration file.
+ *
+ * @author Travis Anthony Torres
+ * @version February 12, 2017
  */
 
 public final class ConfigurationsReader {
     private static Configurations configs = null;
 
+    /*
+     *  Configuration access keys
+     */
     public final static String API_V3_KEY = "configurations.movieDbApi.apiKeyV3";
     public final static String API_V4_KEY = "configurations.movieDbApi.apiKeyV4";
 
-    public static String getApiKey(Resources resource) {
-        return getConfig(resource, API_V3_KEY);
+    /**
+     * Retrieves the API V3 Key for use when requesting content from Movie DB API.
+     *
+     * @param resources Activity resource object.
+     *
+     * @return The v3 api key.
+     */
+    public static String getApiKey(Resources resources) {
+        return getConfig(resources, API_V3_KEY);
     }
 
-    public static String getApiV4Key(Resources resource) {
-        return getConfig(resource, API_V4_KEY);
+    /**
+     * Retrieves the API Key for accessing the Movie DB API v4 features (if utilized).
+     *
+     * @param resources Activity resource object.
+     *
+     * @return The v4 api key.
+     */
+    public static String getApiV4Key(Resources resources) {
+        return getConfig(resources, API_V4_KEY);
     }
 
+    /**
+     * Acquires the value specified for a specific configuration.
+     *
+     * @param resources Activity resource object.
+     * @param configName The configuration key for the desired setting.
+     *
+     * @return The value of the configuration.
+     */
     public static String getConfig(Resources resources, String configName) {
-        //  acquire the configs
+        //  acquire the configs, only one should exist in memory to ensure consistency
         if (configs == null) {
             configs = getConfigurationsList(resources);
         }
@@ -40,6 +70,13 @@ public final class ConfigurationsReader {
         return configs.get(configName);
     }
 
+    /**
+     * Acquires a collection containing all the specified configurations for the project.
+     *
+     * @param resources Activity resource object.
+     *
+     * @return Collection object containing all of the required configurations for the project.
+     */
     private static Configurations getConfigurationsList(Resources resources) {
         XmlResourceParser parser = resources.getXml(R.xml.configurations);
         Configurations configurations = null;
