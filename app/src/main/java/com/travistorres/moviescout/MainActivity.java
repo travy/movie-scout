b6f544efd39e8c9aa68b70a5055ddbf3f83fce6b
Travis Anthony Torres <travis@travistorres.com>
2017-02-12 05:13:57 -0500

    feat:  URL Builder
    Created a URL constructor for the Movie DB API calls.  This class will be
    responsible for ensuring that only valid URL's are called within the
    application.  Provides URL's for sorting by popularity and rating.
    
    See also #4

--- app/src/main/java/com/travistorres/moviescout/MainActivity.java
+++ app/src/main/java/com/travistorres/moviescout/MainActivity.java
@@ -1,13 +1,38 @@
+/**
+ * Copyright (C) 2017 Travis Anthony Torres
+ */
+
 package com.travistorres.moviescout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.travistorres.moviescout.utils.networking.MovieDbUrlBuilder;
+
+import java.net.URL;
+
+/**
+ * TODO:  document the class
+ */
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * TODO:  document method
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  TODO:  remove code below, used only for testing networking api
+        URL ratingMovieUrl = MovieDbUrlBuilder.getRatingsMoviesUrl();
+        TextView ratingView = (TextView) findViewById(R.id.popular_movie_url);
+        ratingView.setText(ratingMovieUrl.toString());
+        URL popularMovieUrl = MovieDbUrlBuilder.getPopularMoviesUrl();
+        TextView popularView = (TextView) findViewById(R.id.rating_movie_url);
+        popularView.setText(popularMovieUrl.toString());
     }
 }
