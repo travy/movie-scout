b0a9244dc30620d83a889fce409a454c07c133a1
Travis Anthony Torres <travis@travistorres.com>
2017-04-02 02:20:40 -0400

    chore:  Imports
    Removed unnecessary ViewCompat import statement

--- app/src/main/java/com/travistorres/moviescout/MovieInfoActivity.java
+++ app/src/main/java/com/travistorres/moviescout/MovieInfoActivity.java
@@ -6,7 +6,6 @@ package com.travistorres.moviescout;
 
 import android.content.Intent;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
