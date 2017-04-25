78444bb2cb79ffa56d18b897f6a40c102a32e1c2
Travis Anthony Torres <travis@travistorres.com>
2017-04-24 21:48:47 -0400

    feat:  Scheduler
    Scheduled a Job when the application is first run.

---
+++ app/src/main/java/com/travistorres/moviescout/notifications/NotificationsUtils.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (C) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.moviescout.notifications;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.travistorres.moviescout.MainActivity;
+import com.travistorres.moviescout.R;
+
+/**
+ * NotificationUtils
+ *
+ * Responsible for displaying notifications that occur within the application to the user even
+ * if they are not currently running the app.
+ *
+ * @author Travis Anthony Torres
+ * @version April 24, 2017
+ */
+
+public class NotificationsUtils {
+    /**
+     * Acquires the task id for the notification pipeline.
+     *
+     * @param context
+     *
+     * @return The numeric identifier for the notification.
+     */
+    private static int getScheduledUpdatesTaskId(Context context) {
+        Resources resources = context.getResources();
+        int scheduledUpdatesTaskId = resources.getInteger(R.integer.favorite_movies_scheduled_updates_task_id);
+
+        return scheduledUpdatesTaskId;
+    }
+
+    /**
+     * Constructs a content.
+     *
+     * @param context
+     *
+     * @return
+     */
+    private static PendingIntent contentIntent(Context context) {
+        int favoritesIntentId = getScheduledUpdatesTaskId(context);
+        Intent startActivityIntent = new Intent(context, MainActivity.class);
+
+        return PendingIntent.getActivity(
+                context,
+                favoritesIntentId,
+                startActivityIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+    }
+
+    /**
+     * Constructs a large icon for the notification window.
+     *
+     * @param context
+     *
+     * @return Large image icon
+     */
+    private static Bitmap largeIcon(Context context) {
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.youtube_icon);
+        return largeIcon;
+    }
+
+    /**
+     * Informs the user that one or movies in their favorites have been updated.
+     *
+     * @param context
+     */
+    public static void notifyUserThatFavoritesHaveBeenUpdated(Context context) {
+        //  builds a notification alerting the user that movies have been updated
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context);
+        notificationBuilder
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.youtube_icon)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.favorites_update_title_notification))
+                .setContentText(context.getString(R.string.favorites_update_description_notification))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);
+
+        //  checks the version of the os to see if priority can be changed
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notificationBuilder.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+        //  displays the notification
+        int favoritesTaskId = getScheduledUpdatesTaskId(context);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(favoritesTaskId, notificationBuilder.build());
+    }
+}
---
+++ app/src/main/java/com/travistorres/moviescout/utils/UpdateFavoritesServiceUtils.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2017 Travis Anthony Torres
+ */
+
+package com.travistorres.moviescout.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import com.travistorres.moviescout.R;
+import com.travistorres.moviescout.services.UpdateFavoritesInformationJobService;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * UpdateFavoritesServiceUtils
+ *
+ * Sets up a service which will determine if any of the users Favorite Moves have been updated since
+ * they were last viewed.
+ *
+ * @author Travis Anthony Torres
+ * @version April 24, 2017
+ */
+
+public class UpdateFavoritesServiceUtils {
+    private static final int REMINDER_INTERVAL_MINUTES = 2;//15;
+    private static final int REMINDER_INTERVAL_SECONDS = (int) (TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES));
+    private static final int SYNC_FLEXTIME_SECONDS = REMINDER_INTERVAL_SECONDS;
+
+    //  specifies if the app has been updated
+    private static boolean sInitialized;
+
+    /**
+     * Runs a concurrent thread for determining if any of the users Favorites have been updated.
+     *
+     * @param context
+     */
+    synchronized public static void scheduleUpdateFavorites(@NonNull final Context context) {
+        //  ignore if the service is already initialized
+        if (sInitialized) {
+            return;
+        }
+
+        //  configures the service for updating the favorites
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+        Job constraintFavoritesUpdateJob = dispatcher.newJobBuilder()
+                .setService(UpdateFavoritesInformationJobService.class)
+                .setTag(context.getString(R.string.favorites_update_job_task))
+                .setConstraints(Constraint.DEVICE_CHARGING)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(
+                        REMINDER_INTERVAL_SECONDS,
+                        REMINDER_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS
+                ))
+                .setReplaceCurrent(true)
+                .build();
+
+        // schedule the service
+        dispatcher.schedule(constraintFavoritesUpdateJob);
+        sInitialized = true;
+    }
+}
